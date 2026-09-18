9c418c19b359734a9490bd8b2374feeadf7811f0
trinhtranphuongtuan8735-oss <trinhtranphuongtuan8735@gmail.com>
2026-09-18 19:38:50 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id 'fabric-loom' version '1.7-SNAPSHOT'
-    id 'maven-publish'
+    id("fabric-loom") version "1.7-SNAPSHOT"
+    id("maven-publish")
 }
 
 version = "1.0.0"
@@ -8,14 +8,20 @@ group = "com.example.addon"
 
 repositories {
     mavenCentral()
-    maven { name = "meteor-dev"; url = "https://maven.meteordevelopment.meteorclient.com/releases" }
-    maven { name = "meteor-snapshots"; url = "https://maven.meteordevelopment.meteorclient.com/snapshots" }
+    maven {
+        name = "meteor-dev"
+        url = uri("https://maven.meteordevelopment.meteorclient.com/releases")
+    }
+    maven {
+        name = "meteor-snapshots"
+        url = uri("https://maven.meteordevelopment.meteorclient.com/snapshots")
+    }
 }
 
 dependencies {
-    minecraft "com.mojang:minecraft:1.21.1"
-    mappings "net.fabricmc:yarn:1.21.1+build.1:v2"
-    fabricApi "net.fabricmc.fabric-api:fabric-api:0.102.0+1.21.1"
+    minecraft("com.mojang:minecraft:1.21.1")
+    mappings("net.fabricmc:yarn:1.21.1+build.1:v2")
+    fabricApi("net.fabricmc.fabric-api:fabric-api:0.102.0+1.21.1")
 
-    modImplementation "meteordevelopment:meteor-client:0.5.8-1.21.1"
+    modImplementation("meteordevelopment:meteor-client:0.5.8-1.21.1")
 }
