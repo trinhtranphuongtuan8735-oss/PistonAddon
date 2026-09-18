70b3033ba9400dd140287d77d2f2f9402bc4dc78
trinhtranphuongtuan8735-oss <trinhtranphuongtuan8735@gmail.com>
2026-09-18 21:03:30 +0700

    Update CommandExample.java

--- src/main/java/com/example/addon/commands/CommandExample.java
+++ src/main/java/com/example/addon/commands/CommandExample.java
@@ -18,7 +18,7 @@ public class CommandExample extends Command {
 
     @Override
     public void build(LiteralArgumentBuilder<ClientSuggestionProvider> builder) {
-        builder.executes(_ -> {
+        builder.executes(context -> {
             info("hi");
             return SINGLE_SUCCESS;
         });
