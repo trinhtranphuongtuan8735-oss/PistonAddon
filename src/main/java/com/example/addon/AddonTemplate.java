94054ac037d307a8f5efb5da0e2847e7a63a1a21
trinhtranphuongtuan8735-oss <trinhtranphuongtuan8735@gmail.com>
2026-09-18 19:32:41 +0700

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,35 +1,20 @@
 package com.example.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
-import com.mojang.logging.LogUtils;
-import meteordevelopment.meteorclient.addons.GithubRepo;
+import com.example.addon.modules.PistonCrystal;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class AddonTemplate extends MeteorAddon {
-    public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+public class Addon extends MeteorAddon {
+    public static final Logger LOG = LoggerFactory.getLogger("Addon");
+    public static final Category CATEGORY = new Category("PistonPvP");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
-
-        // Modules
-        Modules.get().add(new ModuleExample());
-
-        // Commands
-        Commands.add(new CommandExample());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
+        LOG.info("Khoi tao PistonCrystal Addon!");
+        Modules.get().add(new PistonCrystal());
     }
 
     @Override
@@ -40,10 +25,5 @@ public class AddonTemplate extends MeteorAddon {
     @Override
     public String getPackage() {
         return "com.example.addon";
-    }
-
-    @Override
-    public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
     }
 }
